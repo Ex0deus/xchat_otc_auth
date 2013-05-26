59497c937ef192c9322f2c613e4e702dc4f3a164
exodeus <exodeus@digitalfrost.net>
2013-05-26 14:23:16 -0400

    Some minor fixes

--- otc_auth.py
+++ otc_auth.py
@@ -2,7 +2,8 @@
 # OTC Authentication Python Module for XChat
 # =========================================================
 # By: Exodeus
-# Version 0.1 completed 05.26.2013
+# Version 0.1.1 completed 05.26.2013
+#
 
 import xchat
 import gnupg
@@ -11,17 +12,19 @@ import cStringIO
 from time import sleep
 
 # Define our module.
-__module_name__ = "OTC Authentication"
-__module_version__ = "0.1"
+__module_name__ = "Ex0's OTC Authentication Tool"
+__module_version__ = "0.1.1"
 __module_author__ = "Exodeus"
 __module_description__ = "This is a handy tool for XChat2 IRC client used for OTC Authentication"
 
+xchat.prnt("%s Ver. %s : LOADED" % (__module_name__, __module_version__))
 
 # Print Version Information
 def otcauth_ver():
 	xchat.prnt("%s Version: %s By: %s" % (__module_name__, __module_version__, __module_author__))
 	xchat.prnt(__module_description__)
-
+	
+	return xchat.EAT_ALL
 
 # Print out the Help info
 def otcauth_help(topic):
@@ -29,6 +32,8 @@ def otcauth_help(topic):
 		xchat.prnt(""" OTCAUTH Tool Help page not completed! """)
 	else:
 		xchat.prnt("I don't know anything about topic: %s" % topic)
+	
+	return xchat.EAT_ALL
 
 # Get our string to decrypt from gribble
 def get_auth(nick, keyid):
@@ -60,16 +65,18 @@ def otc_decrypt(encrypted_string):
 
 	xchat.command("MSG gribble everify %s" % (str(auth_string))
 	
+	return xchat.EAT_ALL
+	
 # The callback function to our hook that ties it all together	
 def otcauth_cb(*args):
 	check = str(args[0][1])
-	print args
+	print args 	#FOR DEBUGGING PURPOSES
 	if check == "help":
 		otcauth_help(args[0][2:])
 	elif check == "version":
 		otcauth_ver()
 	elif check == "auth":
-		xchat.prnt("Got to auth")
+		xchat.prnt("Got to auth") # For debugging purposes
 		nick = xchat.get_info('nick')
 		if len(args[0][2]) != 16:
 			xchat.prnt("You need to supply a valid key id!")
@@ -77,8 +84,10 @@ def otcauth_cb(*args):
 			keyid = args[0][2]
 			otc_decrypt(get_auth(nick, keyid))
 	else:
-		xchat.prnt("here at the else")
+		xchat.prnt("here at the else") # For debugging purposes
 		otcauth_help("basic")
-
+	
+	return xchat.EAT_NONE
+	
 # Hook our functions to commands to make them usable
 xchat.hook_command("OTCAUTH", otcauth_cb, help="Type /OTCAUTH help for more help")
